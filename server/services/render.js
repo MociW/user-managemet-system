fd6159cf29bed1cf70c51062cd1f8ed52e41cb29
MociW <mochixmoca@gmail.com>
2022-12-07 19:22:37 +0700

    update ui

--- server/services/render.js
+++ server/services/render.js
@@ -1,9 +1,9 @@
 const axios = require('axios');
-
+const PORT = process.env.PORT || 8080;
 exports.homeRoutes = (req, res) => {
     // Make a get request to /api/users
     axios
-        .get('http://localhost:3200/api/users')
+        .get(`http://localhost:${PORT}/api/users`)
         .then(function (response) {
             res.render('index', { users: response.data });
         })
@@ -18,7 +18,7 @@ exports.add_user = (req, res) => {
 
 exports.update_user = (req, res) => {
     axios
-        .get('http://localhost:3200/api/users', { params: { id: req.query.id } })
+        .get(`http://localhost:${PORT}/api/users`, { params: { id: req.query.id } })
         .then(function (userdata) {
             res.render('update_user', { user: userdata.data });
         })
